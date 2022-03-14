3c53a98e24211aec1e4a32bf43229301b52c0a45
Le Gany <leganybit@protonmail.com>
2022-03-14 21:47:32 +0100

    first commit

---
+++ albertoduvall.py
@@ -0,0 +1,9 @@
+from website import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+	#(debug=True)
+	app.run(debug=True, host="127.0.0.1", port=5003)
+
+
---
+++ website/__init__.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+
+def create_app():
+	app = Flask(__name__)
+
+	@app.route("/")
+	def Firma():
+		return render_template("/firma.html")
+	@app.route("/carrera")
+	def carrera():
+		return render_template("/carrera.html")
+	@app.route("/criticas")
+	def criticas():
+		return render_template("/criticas.html")
+
+	@app.route("/realismo")
+	def realismo():
+		return render_template("/obras/realismo.html")
+	@app.route("/realismo_onirico")
+	def realismo_onirico():
+		return render_template("/obras/realismo_onirico.html")
+	@app.route("/suprarealismo")
+	def suprarealismo():
+		return render_template("/obras/suprarealismo.html")
+	@app.route("/retratos")
+	def retratos():
+		return render_template("/obras/retratos.html")
+
+	@app.route("/contacto")
+	def contacto():
+		return render_template("/contacto.html")
+
+	return app
